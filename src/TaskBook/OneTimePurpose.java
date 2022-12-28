712e1f2b898ea186a36cae5f655855f556b171f7
Дом <ira-rostov80@mail.ru>
2022-12-28 10:40:21 +0300

    первый коммит-1

---
+++ src/TaskBook/OneTimePurpose.java
@@ -0,0 +1,21 @@
+package TaskBook;
+
+import java.time.LocalDateTime;
+
+public class OneTimePurpose extends Purpose implements Repeatable {
+
+    public OneTimePurpose(String title, String note, TypeOfPurpose type, LocalDateTime date) {
+        super(title, note, type, date);
+    }
+
+    @Override
+    public LocalDateTime getDateTime(LocalDateTime dateTime) {
+        return dateTime;
+    }
+
+    @Override
+    public String getTypeRepeatable() {
+        return "Однократная ";
+    }
+
+}
